09b537ce0996faa58eae21d48484a610181aeee9
Andy Pearson <andy@londonparkour.com>
2020-09-30 17:27:25 +0100

    Initial Commit

---
+++ andyp_oauth.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * @wordpress-plugin
+ * Plugin Name:       _ANDYP - OAUTH
+ * Plugin URI:        http://londonparkour.com
+ * Description:       <strong>🔌PLUGIN</strong> | <em>ANDYP > Universal Exporter</em> | oAuth for 3rd Parties.
+ * Version:           1.0.0
+ * Author:            Andy Pearson
+ * Author URI:        https://londonparkour.com
+ * Domain Path:       /languages
+ */
+
